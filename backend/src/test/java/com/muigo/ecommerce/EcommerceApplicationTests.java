b45c3219278c174cea7f74d2321e687db9cfcf90
Dennis Muigo <dmuigo@gmail.com>
2026-04-09 12:48:37 +0300

    Included annotation @activeProfile to test classes and renamed application.properties to application-test.properties

--- backend/src/test/java/com/muigo/ecommerce/EcommerceApplicationTests.java
+++ backend/src/test/java/com/muigo/ecommerce/EcommerceApplicationTests.java
@@ -1,13 +1,15 @@
-//package com.muigo.ecommerce;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class EcommerceApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+package com.muigo.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class EcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
