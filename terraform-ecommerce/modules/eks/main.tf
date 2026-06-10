fac86a083e578ba9d5f43e9ce688f4aa2f682355
Dennis Muigo <dmuigo@gmail.com>
2026-06-10 23:18:08 +0300

    changed secrets from base64 encoded

--- terraform-ecommerce/modules/eks/main.tf
+++ terraform-ecommerce/modules/eks/main.tf
@@ -49,4 +49,6 @@ module "eks" {
 # ── Outputs ───────────────────────────────────────────────────────────────────
 output "cluster_name"     { value = module.eks.cluster_name }
 output "cluster_endpoint" { value = module.eks.cluster_endpoint }
-output "cluster_version"  { value = module.eks.cluster_version }
+
+output "cluster_security_group_id" { value = module.eks.cluster_security_group_id }
+
